ab6da544aa1ca9b58a1227e6da12abf6e4b5b686
Leandro Souza <161175460+lsouzadasilva@users.noreply.github.com>
2025-04-14 18:30:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if st.session_state["authentication_status"]:
     st.sidebar.markdown("""
         **Desenvolvido por Leandro Souza**  
         [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/leandro-souza-bi/)
-        [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/lsouzadasilva/streamlit_openai_langchain_read_pdf)
+        [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)]('')
         """)
 
     if paginas == "My Trainnig":
