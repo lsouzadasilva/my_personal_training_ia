598160e128e7fa4f35baca9396ee92f782399f7a
Leandro Souza <161175460+lsouzadasilva@users.noreply.github.com>
2025-04-28 22:09:37 -0300

    Update training.py

--- paginas/training.py
+++ paginas/training.py
@@ -13,7 +13,7 @@ def pagina_inicial():
         f"""
         <h1 style="display: flex; align-items: center;">
         Personal Trainer
-            <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People%20with%20activities/Person%20Lifting%20Weights%20Medium-Dark%20Skin%20Tone.png" alt="Personal Trainer" style="width: 50px; margin-right: 10px;">
+            <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People%20with%20activities/Person%20Lifting%20Weights%20Medium-Dark%20Skin%20Tone.png" alt="Personal Trainer" style="width: 30px; margin-right: 10px;">
         </h1>
         <hr>
         """,
