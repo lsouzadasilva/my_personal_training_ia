84b35d99cac89edce04847fb507c7003d86487dd
Leandro Souza <lsouzadasilva4@gmail.com>
2025-06-19 21:35:59 -0300

    Update training.py

--- paginas/training.py
+++ paginas/training.py
@@ -82,3 +82,4 @@ def pagina_inicial():
     if all(st.session_state.status_exercicios):
         if st.button('🏁 Finalizar Treino'):
             st.success(f"Parabéns! Você finalizou o treino de {treino_escolhido} 🎉")
+            st.balloons()
