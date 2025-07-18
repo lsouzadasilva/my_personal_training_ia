c45c6eedcc1faa5fde2d21beefbc173ac6ab9bd3
Leandro Souza <lsouzadasilva4@gmail.com>
2025-07-18 14:11:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if st.session_state["authentication_status"]:
     st.sidebar.divider()
     st.sidebar.markdown("""
         **Desenvolvido por Leandro Souza**  
-        [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in//leandro-souza-dados/)
+        [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/leandro-souza-dados/)
         [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)]('')
         """)
 
